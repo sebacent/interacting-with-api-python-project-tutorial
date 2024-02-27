f90cbb569ef126c9773fca7a939928891b9f2d7c
Sebastián Centurión <sebastiancenturion27@gmail.com>
2024-02-27 01:45:20 +0000

    Completo solución

--- src/app.py
+++ src/app.py
@@ -2,14 +2,24 @@ import os
 from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
-
+import seaborn as sns
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 # load the .env file variables
 load_dotenv()
+SPOTIPY_CLIENT_ID = os.environ.get("CLIENT_ID")
+SPOTIPY_CLIENT_SECRET = os.environ.get("TOKEN")
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,))
+artist_id = 'spotify:artist:53XhwfbYqKCa1cC15pYq2q'
+results = spotify.artist_top_tracks(artist_id)
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+tracks_df= pd.DataFrame.from_records(results['tracks'][:10])
+tracks_df.sort_values(["popularity"], inplace = True)
+print(tracks_df.head(5)['duration_ms'])
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+scatter_plot = sns.scatterplot(data = tracks_df, x = "popularity", y = "duration_ms")
+fig = scatter_plot.get_figure()
+fig.savefig("scatter_plot.png")
+#Como se puede ver en scatter_plot.png, no existe una relación entre la duración de la canción y la popularidad de la misma.
